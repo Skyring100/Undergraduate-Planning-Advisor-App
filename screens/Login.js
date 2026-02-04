8f067fa2f3b8e191688cc82cc728e99432e4ea94
CoryStecyk <corystecyk@gmail.com>
2026-02-03 20:39:54 -0800

    added import to login.js

--- screens/Login.js
+++ screens/Login.js
@@ -2,3 +2,7 @@
 It will have two buttons one for login and one for signup. 
 On clicking signup it will navigate to Register page.
 On clicking login it will validate credentials and navigate to Dashboard page.*/
+
+
+
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
