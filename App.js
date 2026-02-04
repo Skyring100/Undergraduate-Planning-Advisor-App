33139a0ce0ddf5a8e1c05fc3f588d69b7a0e9bc3
CoryStecyk <corystecyk@gmail.com>
2026-02-03 22:20:43 -0800

    Reset app.js and Login.js due to errords and not understand how navigation works

--- App.js
+++ App.js
@@ -1,26 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginView from './screens/Login';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen 
-          name="Login" 
-          component={LoginView}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-          name="Course" 
-          component={CurrentCourses}
-          options={{ title: 'Course' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  
 }
 
 const styles = StyleSheet.create({
