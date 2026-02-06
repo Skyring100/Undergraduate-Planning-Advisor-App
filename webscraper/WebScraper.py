b3aaa631577ca1c0466ed425ddab88354a248f55
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2026-02-05 16:03:10 -0800

    Reorganized web scraper code into separate function for driver

--- webscraper/WebScraper.py
+++ webscraper/WebScraper.py
@@ -5,15 +5,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def get_course_information(courseURL):
+def create_webdriver() -> webdriver.Chrome:
     driver_options = Options()
     driver_options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=driver_options)
+    return driver
+
+
+def get_course_information(driver: webdriver.Chrome, courseURL: str):
     driver.get(courseURL)
-
-
     # Wait until dynamic course elements load
     WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//section[@id='results']/ul/li")))
+
+    # Find the results section
     mainEle = driver.find_element(By.ID, "results")
 
     title = mainEle.find_element(By.TAG_NAME, "header").text
@@ -26,8 +30,8 @@ def get_course_information(courseURL):
     for prereqEle in prereqElements:
         prereqs.append(prereqEle.text)
 
-    driver.quit()
     print(f"Course Name: {title} Prereqs: {prereqs}\nDescription:\n{desc}")
 
-
-get_course_information('https://tools.unbc.ca/course-catalogue?subj=CPSC&crse=100')
+scraper = create_webdriver()
+get_course_information(scraper,'https://tools.unbc.ca/course-catalogue?subj=CPSC&crse=100')
+scraper.quit()
