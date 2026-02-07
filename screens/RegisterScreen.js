0783feaf954304cc8dc426bc2f69184692968445
CoryStecyk <corystecyk@gmail.com>
2026-02-06 20:04:34 -0800

    Register screen WIP

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -2,23 +2,48 @@
 Degree program and start year will be dropdowns.
 On clicking register it will validate inputs and navigate to Dashboard page.*/
 
+import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { TouchableOpacity, Text, StyleSheet } from "react-native"
+import { TouchableOpacity, Text, StyleSheet, View, TextInput, Dimensions } from "react-native"
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { useUserStore } from '../contexts/UserContext';
+import RegisterButton from '../components/RegisterButton';
 
+const screenWidth = Dimensions.get('window').width;
+const inputWidth = screenWidth * 0.85; // 85% of screen
 
 export default function RegisterScreen() {
 
     const navigation = useNavigation();
+    
+    const { setUser } = useUserStore();
+    const [emailInput, setEmailInput] = useState('');
+    const [password, setPassword] = useState('');
 
     return(
         <SafeAreaProvider>
             <SafeAreaView style={{flexDirection: 'column', padding: 10, flex: 1}}>
-                <TouchableOpacity style={styles.button} onPress={()=>{navigation.navigate('Login',{})}}>
-                    <Text style={styles.buttonText}>
-                        Go back to Login page (Submit button)
-                    </Text>
-                </TouchableOpacity>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>Register</Text>
+                </View>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter your email"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    value={emailInput}
+                    onChangeText={setEmailInput}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter your password"
+                    secureTextEntry={true}
+                    value={password}
+                    onChangeText={setPassword}
+                />
+
+                <RegisterButton style={styles.button} onPress={()=>{navigation.navigate('Login',{})}}/>
+
             </SafeAreaView >
         </SafeAreaProvider>
     );
@@ -26,18 +51,37 @@ export default function RegisterScreen() {
 
 
 const styles = StyleSheet.create({
-    button: {
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        alignContent: 'center',
+        margin: 'auto',
+    },
+    titleContainer: {
+        flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#035642',
-        padding: 10,
-        margin: 'auto',
-        borderRadius: 20,
-        width: 'auto',
-        height: 'auto',
+        marginBottom: 30,
+        padding: 80
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
     },
     buttonText: {
         color: '#fff',
+    },
+    input: {
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 8,
+        paddingHorizontal: 15,
+        fontSize: 16,
+        backgroundColor: '#f9f9f9',
+        marginTop: 10,
+        width: inputWidth
     },
 });
 
