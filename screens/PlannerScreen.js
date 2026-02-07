f05d78121adfa2c17d4feaedbdc6773a16446423
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2026-02-07 09:28:04 -0800

    Added scrollview logic to work with any number of courses per semester

--- screens/PlannerScreen.js
+++ screens/PlannerScreen.js
@@ -2,20 +2,26 @@
 It will have a drop down for selecting which one the user wants to view.
 It will show the courses in a table format.*/
 
-import { View, StyleSheet, Text, Dimensions } from 'react-native';
+import { View, StyleSheet, Text, ScrollView } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import CourseButton from '../components/CourseButton';
 import { Course } from '../data_models/Course';
 
-const screenWidth = Dimensions.get('window').width;
-const inputWidth = screenWidth * 0.5; // 85% of screen
 
 export default function PlannerScreen() {
+    const semester1Courses = [
+        new Course("CPSC100", "Computer Programming", "Learn basic programmin", []), 
+        new Course("CPSC141", "Discrete Mathematics", "Comp sci math", [])
+    ];
+    const semester2Courses = [
+        new Course("CPSC101", "Computer Programming 2", "Learn object oriented stuff in Java", ["CPSC100"]),
+        new Course("CPSC242", "Discrete Math 2", "Even more comp sci math", ["CPSC141"])
+    ];
 
     return(
         <SafeAreaProvider>
             <SafeAreaView style={{flexDirection: 'column', padding: 10,flex: 1}}>
-                <YearSection yearNumber={1} sem1Courses={new Course("CPSC100", "Computer Programming", "Learn basic programmin", [])} sem2Courses={new Course("CPSC101", "Computer Programming 2", "Learn object oriented stuff in Java", ["CPSC100"])}></YearSection>
+                <YearSection yearNumber={1} sem1Courses={semester1Courses} sem2Courses={semester2Courses}></YearSection>
             </SafeAreaView >
         </SafeAreaProvider>
     );
@@ -23,17 +29,32 @@ export default function PlannerScreen() {
 
 
 function YearSection({yearNumber, sem1Courses, sem2Courses}) {
+
     return (
         <View>
             <Text style={styles.yearHeader}>Year {yearNumber}</Text>
             <View style={styles.yearContent}>
                 <View>
                     <Text style={styles.semesterHeader}>Semester 1</Text>
-                    <CourseButton course={sem1Courses}></CourseButton>
+                    <ScrollView>
+                        {
+                            sem1Courses.map(c=>(
+                                <View key={c.id}>
+                                    <CourseButton course={c}></CourseButton>
+                                </View>
+                            ))
+                        }
+                    </ScrollView>
                 </View>
                 <View>
                     <Text style={styles.semesterHeader}>Semester 2</Text>
-                    <CourseButton course={sem2Courses}></CourseButton>
+                    <ScrollView>
+                        {
+                            sem2Courses.map(c=>(
+                                <CourseButton course={c}></CourseButton>
+                            ))
+                        }
+                    </ScrollView>
                 </View>
             </View>
         </View>
