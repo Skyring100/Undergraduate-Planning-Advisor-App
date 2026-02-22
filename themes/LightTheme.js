634fecdfed23f3d80d3f59450a9034c54c813497
liz <lize6012@gmail.com>
2026-02-21 16:35:22 -0800

    made a theme folder with colors for green, red, blue, pink, purple, yellow, and orange

---
+++ themes/LightTheme.js
@@ -0,0 +1,12 @@
+//green, red, blue, pink, purple, yellow, orange
+const mainColor = ['#09F6BF', '#F60909', '#0997F6', '#F6099B', '#AB09F6', '#F6E609', '#F68009'];
+const secondColor = ['#35F8CA', '#F83535', '#35AAF8', '#F835AD', '#BA35F8', '#F8EB35', '#F89635'];
+const thirdColor = ['#62F9D6', '#F96262', '#62BDF9', '#F962BF', '#C962F9', '#F9EF62', '#F9AD62'];
+const fourthColor = ['#8EFBE1', '#FB8E8E', '#8ECFFB', '#FB8ED1', '#D88EFB', '#FBF48E', '#FBC48E'];
+
+export const LightTheme = {
+    mainColor: mainColor,
+    secondColor: secondColor,
+    thirdColor: thirdColor,
+    fourthColor: fourthColor,
+};
