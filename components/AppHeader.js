efe592cbaef3164f4b896e862251e272c529124a
CoryStecyk <corystecyk@gmail.com>
2026-02-24 18:40:39 -0800

    made app logo in header

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -16,7 +16,7 @@ export default function AppHeader() {
         <View style={[styles.header, {backgroundColor: mainDark[indexColour]}]}>
             <HelpButton onPress={()=> {alert("Make this button give info about current page")}}/>
             <Text style={styles.headerText}>Gradian</Text>
-            <Image style={{justifyContent:'center', alignItems:'center', marginTop: 35, marginBottom: 5}} source={require('../assets/favicon.png')}/>
+            <Image style={{justifyContent:'center', alignItems:'center', marginTop: 35, marginBottom: 5, resizeMode: 'center', width: 80, height: 80}} source={require('../assets/main-logo.png')}/>
             <SettingsButton />
         </View>
         
