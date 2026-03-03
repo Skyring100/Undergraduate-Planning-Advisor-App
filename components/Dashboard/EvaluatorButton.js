3c3d50073b703c14dd88b44525596bf9d1c79c8c
liz <lize6012@gmail.com>
2026-03-02 18:28:12 -0800

    pie chart is implemented

--- components/Dashboard/EvaluatorButton.js
+++ components/Dashboard/EvaluatorButton.js
@@ -1,7 +1,11 @@
 import { useNavigation } from '@react-navigation/native';
-import { TouchableOpacity, Text, StyleSheet, Dimensions, View } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, Dimensions, View, Pressable } from 'react-native';
 import { useThemeStore, mainDark, mainLight, fourthLight, borderColour} from '../../contexts/ThemeContext';
+import { useState } from 'react';
 import OutlinedText from '@kdn0325/react-native-outlined-text';
+import possibleCourses from '../../data/possible_courses.json'
+import completedCourses from '../../data/completed_courses.json'
+import PieChart from "react-native-pie-chart";
 
 const screenHeight = Dimensions.get('window').height;
 const buttonHeight = screenHeight *0.27;
@@ -11,20 +15,34 @@ const buttonWidth = buttonHeight;//screenWidth * 0.60;
 export default function EvaluatorButton() {
     const navigation = useNavigation();
     const { isDarkMode, indexColour } = useThemeStore();
+    
+    const pieChartPercent = (completedCourses.length/possibleCourses.length)*100;
+    //const [percentage, setPercentage] = useState(pieChartPercent+"%");
 
     const handlePress = () => {
         navigation.navigate('Evaluator',{})
     };
     
     return (
-        <TouchableOpacity
-            style={[styles.button, 
-                {backgroundColor: isDarkMode ? mainDark[indexColour] : mainLight[indexColour], 
-                    borderColor: isDarkMode ? borderColour[indexColour] : mainDark[indexColour]}]}
+        <Pressable
+            style={[styles.button, {borderColor: isDarkMode ? borderColour[indexColour] : mainDark[indexColour]}]}
             onPress={handlePress}
-            activeOpacity={1}
-        >
-            <View pointerEvents="none">
+            //activeOpacity={1}
+        >   
+            <PieChart
+                widthAndHeight={buttonHeight*0.96}
+                series={[
+                    {   value: 100-parseFloat(pieChartPercent), 
+                        color: isDarkMode ? borderColour[indexColour] : mainDark[indexColour]
+                    },
+                    {   value: parseFloat(pieChartPercent), 
+                        color: isDarkMode ? mainDark[indexColour] : mainLight[indexColour] 
+                    },
+                    
+                ]}
+            />
+            <View pointerEvents="none" style = {{position: 'absolute'}}>
+                
                 <OutlinedText
                     text={' Evaluator '}
                     color={isDarkMode ? fourthLight[indexColour] : '#ffffff'}
@@ -35,7 +53,7 @@ export default function EvaluatorButton() {
                 />
             </View>
             {/* <Text style={[styles.buttonText, {color: isDarkMode ? fourthLight[indexColour] : mainDark[indexColour]}]}>Evaluator</Text> */}  
-        </TouchableOpacity>
+        </Pressable>
     );
 }
 const styles = StyleSheet.create({
