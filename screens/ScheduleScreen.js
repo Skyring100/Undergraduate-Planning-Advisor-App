8e1ea81596ab0ee62f921c5a848bc5dcc41dbdb7
MateusPAbreu <122947765+MateusPAbreu@users.noreply.github.com>
2026-03-05 18:40:10 -0300

    Data displayed for schedule

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -2,7 +2,7 @@
 It will display the courses in a calendar format with time slots.
 Users can click on each course to view more details about it or add times to their selected courses.*/
 import { useState } from 'react';
-import { View, StyleSheet, FlatList, Text, ScrollView } from 'react-native';
+import { View, StyleSheet, Text, ScrollView } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { Calendar, CalendarList, WeekCalendar } from 'react-native-calendars';
 // import { CalendarBody, CalendarContainer, CalendarHeader } from '@howljs/calendar-kit';
@@ -10,33 +10,34 @@ import BackButton from '../components/BackButton';
 import completedCourse from '../data/UNBC_course_data.json';
 
 
-import {useThemeText, useThemeBackground, useThemeStore} from "../contexts/ThemeContext";
-import {useWindowDimensions} from "react-native";
+import { useThemeText, useThemeBackground, useThemeStore } from "../contexts/ThemeContext";
+import { useWindowDimensions } from "react-native";
 
 export default function ScheduleScreen() {
 
     const [selected, setSelected] = useState('');
     const [isVisible, setIsVisible] = useState(false);
-    const [selectedId, setSelectedId] = useState();
+    const [selectedDay, setSelectedDay] = useState('');
+    const [selectedClass, setSelectedClass] = useState('');
 
     const sampleSchedule = [
-        { 'start': '2026-03-23 09:00:00', 'duration': '00:20:00', 'note': 'Walk my dog' },
-        { 'start': '2026-03-24 14:00:00', 'duration': '01:00:00', 'note': 'Doctor\'s appointment' },
-        { 'start': '2026-03-25 08:00:00', 'duration': '00:30:00', 'note': 'Morning exercise' },
-        { 'start': '2026-03-25 14:00:00', 'duration': '02:00:00', 'note': 'Meeting with client' }
+        { 'start': '2026-03-05', 'time': '01:00', 'duration': '00:20:00', 'note': 'CPSC 100' },
+        { 'start': '2026-03-05', 'time': '10:30', 'duration': '01:00:00', 'note': 'Math 100' },
+        { 'start': '2026-03-05', 'time': '2:40', 'duration': '00:30:00', 'note': 'Anth 100' },
+        { 'start': '2026-03-08', 'time': '3:20', 'duration': '02:00:00', 'note': 'Pysch 100' }
     ]
 
     const themeText = useThemeText();
     const themeBg = useThemeBackground();
-    const {isDarkMode} = useThemeStore();
-    const {width, height} = useWindowDimensions();
+    const { isDarkMode } = useThemeStore();
+    const { width, height } = useWindowDimensions();
 
     return (
         <SafeAreaProvider>
-            <SafeAreaView style={{...themeBg, minHeight: height}}>
+            <SafeAreaView style={{ ...themeBg, minHeight: height }}>
 
-                <View style={{alignItems: 'center', justifyContent: 'center', ...themeBg, width: width}}>
-                        <BackButton/>
+                <View style={{ alignItems: 'center', justifyContent: 'center', ...themeBg, width: width }}>
+                    <BackButton />
                 </View>
 
                 <View style={styles.scheduleHeader}>
@@ -48,6 +49,7 @@ export default function ScheduleScreen() {
                     onDayPress={day => {
                         setIsVisible(true);
                         setSelected(day.dateString);
+                        setSelectedDay(day.dateString);
                     }}
                     markedDates={
                         {
@@ -72,16 +74,30 @@ export default function ScheduleScreen() {
     );
 
     function DailyAgenda() {
+        const classes = [];
+        //Mateus: I know I'll need something better later, probably a datastructure that will allow for quick search, maybe a hashtable, but for now this helps to get the idea
+        for (let i = 0; i < sampleSchedule.length; i++) {
+            if (selectedDay == sampleSchedule[i].start) {
+                classes.push(sampleSchedule[i].note + " " + sampleSchedule[i].time + "\n") //how do I show a list/array of things in JSX??
+            }
+        }
+
         if (isVisible) {
             return (
-                <View style={styles.agenda}>
-                    <Text >
-                        {completedCourse.all_courses[0].id}
-                        
-                    </Text>
-                    <Text>
-                        {completedCourse.all_courses[0].title}
-                    </Text>
+                <View style={styles.agendaView}>
+                    <ScrollView>
+                        <View style={styles.agenda}>
+                            <Text >
+                                {selectedDay}
+                                {/* {sampleSchedule.length} */}
+                                {/* {completedCourse.all_courses[0].id} */}
+                            </Text>
+                            <Text>
+                                {/* {selectedDay} */}
+                                {classes}
+                            </Text>
+                        </View>
+                    </ScrollView>
                 </View>
             )
         }
@@ -107,8 +123,11 @@ const styles = StyleSheet.create({
     days: {
         fontSize: 20,
     },
-    agenda:{
-        flexDirection:'column',
+    agendaView: {
+        flex: 1,
+    },
+    agenda: {
+        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
     }
