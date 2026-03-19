69982f17e423fbf03d4fd4f1a74df83ee1bd513f
CoryStecyk <corystecyk@gmail.com>
2026-03-18 23:55:19 -0700

    fixed type in courses

--- app/services/courseService.js
+++ app/services/courseService.js
@@ -1,7 +1,7 @@
 import { API_BASE_URL } from './api';
 
 export const getCourses = async (courseID) => {
-    const url = `${API_BASE_URL}/coruses/${courseID}`;
+    const url = `${API_BASE_URL}/courses/${courseID}`;
     console.log(url)
     const response = await fetch(url, {
         method: 'GET',
