46a99b50603dc03c1dd15e38c076fe0dc2fdf520
CoryStecyk <corystecyk@gmail.com>
2026-03-19 15:37:04 -0700

    revert back to using dummy data isntead of database

--- app/screens/CourseListScreen.js
+++ app/screens/CourseListScreen.js
@@ -17,7 +17,8 @@ import CoursePopUp from '../components/Requistes/CoursePopUp';
 import all_courses from '../data/UNBC_course_data.json'
 
 
-
+const filtered_course_types = [...new Set(all_courses.all_courses.map(course => course.id.trim().split(/\n/)[0]))].sort();
+const course_type = ["All"].concat(filtered_course_types);
 
 export default function CourseListScreen() {
 
@@ -28,20 +29,20 @@ export default function CourseListScreen() {
     const {width, height} = useWindowDimensions();
 
     const [courses, setCourses] = useState(null);
-    const [filteredCourseTypes, setFilteredCourseTypes] = useState([]);
     const [activeType, setActiveType] = useState("All");
 
-    
-
-
+    const filteredCourses = activeType === "All"
+        ? all_courses.all_courses
+        : all_courses.all_courses.filter(course => course.id.trim().startsWith(activeType));
+        
+    {/*
+        
     useEffect(() => {
         getCourses().then((apiResult) => {
             alert("API call was good");
 
             if (apiResult.success){
                 setCourses(apiResult.data);
-                setFilteredCourseTypes([...new Set(courses.map(course => course.course_id.slice(0,4)))].sort());
-                
             }else{
                 alert("API call was unsuccessful");
                 setCourses([]);
@@ -50,14 +51,7 @@ export default function CourseListScreen() {
 
     }, []);
 
-    const course_type = ["All"].concat(filteredCourseTypes);
-
-    const filteredCourses = activeType === "All"
-        ? courses
-        : courses.filter(course => course.course_id.trim().startsWith(activeType));
-        
-        
-    
+    */}
 
     return (
         <SafeAreaProvider>
@@ -90,12 +84,12 @@ export default function CourseListScreen() {
                     renderItem={({item}) => (
                         <View style={styles.contentBackground}>
                             <View style={{backgroundColor: firstColour.backgroundColor, height: 5, width: 'auto'}}/>
-                            <Text style={styles.courseTitle}>{item.course_id}</Text>
+                            <Text style={styles.courseTitle}>{item.id.replace(/\n/g, ' ')}</Text>
                             <Text style={styles.courseText}>{item.title}</Text>
                             <Text style={styles.courseText}>{item.prereq}</Text>
                         </View>
                     )}
-                    keyExtractor={(item) => item.course_id}
+                    keyExtractor={(item) => item.id.toString()}
                     ListFooterComponent={
                         <SafeAreaView style={{marginBottom: 310}}>
                         </SafeAreaView>
--- app/services/courseService.js
+++ app/services/courseService.js
@@ -10,7 +10,7 @@ export const getCourses = async () => {
         },
     });
     const data = await response.json();
-    console.log(data);
+    //console.log(data);
 
     return data;
 };
