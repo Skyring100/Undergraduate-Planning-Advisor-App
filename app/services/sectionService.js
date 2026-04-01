5409cda8f2f72b2323635bcf6452d5853bf85d23
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2026-04-01 13:24:49 -0700

    Fixed section service from hanging when day of week request is made

--- app/services/sectionService.js
+++ app/services/sectionService.js
@@ -1,12 +1,15 @@
 import { API_BASE_URL } from './api.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const getSectionsForCourse = async (courseID) => {
     const url = `${API_BASE_URL}/section/${courseID}`;
     console.log(url);
+    const token = await AsyncStorage.getItem('authToken');
     const response = await fetch(url, {
         method: 'GET',
         headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
         },
     });
     const data = await response.json();
@@ -18,10 +21,12 @@ export const getSectionsForCourse = async (courseID) => {
 export const getSectionsOnDayOfWeek = async (dow) => {
     const url = `${API_BASE_URL}/sections/dow/${dow}`;
     console.log(url);
+    const token = await AsyncStorage.getItem('authToken');
     const response = await fetch(url, {
         method: 'GET',
         headers: {
-        'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
         },
     });
     const data = await response.json();
@@ -33,10 +38,12 @@ export const getSectionsOnDayOfWeek = async (dow) => {
 export const addSections = async (section) => {
     const url = `${API_BASE_URL}/sections/${section}`;
     console.log(section);
+    const token = await AsyncStorage.getItem('authToken');
     const response = await fetch(url, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
         },
     });
     const data = await response.json();
